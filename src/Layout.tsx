23dd6582b308fc1cab25c30f5d7bcc75d02e70fe
Young Seo Kim <youngseokim7@gmail.com>
2025-03-16 13:21:01 +0900

    make root and layout take height:100

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full h-full">
       <h1>navbar here perhaps</h1>
       <Outlet />
       <div className="mt-auto text-3xl">footer</div>
