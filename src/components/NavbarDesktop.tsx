6263541c239ae5d417efd8424d970a666da98ef0
Young Seo Kim <youngseokim7@gmail.com>
2025-03-16 20:05:29 +0900

    routing for desktop navbar

--- src/components/NavbarDesktop.tsx
+++ src/components/NavbarDesktop.tsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function NavbarDesktop() {
   return (
-    <div>
-      <h1 className="hidden lg:block">
-        this should only show on larger screens
-      </h1>
+    <div className="hidden lg:flex justify-between">
+      <div>
+        <Link className="text-3xl" to="/">
+          LOGO
+        </Link>
+      </div>
+      <div className="flex">
+        <div>
+          <Link className="mx-3" to="electronics">
+            Electronics
+          </Link>
+          <Link className="mx-3" to="clothes-f">
+            Women's Clothing
+          </Link>
+          <Link className="mx-3" to="clothes-m">
+            Men's Clothing
+          </Link>
+          <Link className="mx-3" to="jewelery">
+            Jewelery
+          </Link>
+        </div>
+        <div className="mx-2">PLACEHOLDER FOR ICON</div>
+      </div>
     </div>
   );
 }
