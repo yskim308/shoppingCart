b4ebfdbe7e3c47addc611dc2bd9cfe165dbf5f68
Young Seo Kim <youngseokim7@gmail.com>
2025-03-17 13:57:06 +0900

    useEffect to prefilter products before mapping. also add key

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,26 +1,37 @@
 import { useOutletContext, useParams } from "react-router-dom";
 import { Product } from "../types";
 import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
 
 export default function Category() {
+  const [filterWord, setFilterWord] = useState<string>("");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const { category } = useParams();
-  let filterWord = category;
-  if (category === "clothes-m") {
-    filterWord = "men's clothing";
-  } else if (category === "clothes-f") {
-    filterWord = "women's clothing";
-  }
   const products: Product[] = useOutletContext();
-  const filteredProducts = products.filter((product) => {
-    return product.category === filterWord;
-  });
-  console.log(filterWord);
+
+  useEffect(() => {
+    let processedString = category;
+    if (category === "clothes-m") {
+      processedString = "men's clothing";
+    } else if (category === "clothes-f") {
+      processedString = "women's clothing";
+    } else {
+      if (category) processedString = category;
+      else processedString = "";
+    }
+    setFilterWord(processedString);
+
+    const filtered = products.filter((product) => {
+      return product.category === filterWord;
+    });
+    setFilteredProducts(filtered);
+  }, [category, products, filterWord]);
 
   return (
     <div className="flex justify-center p-10 w-full">
-      <div className="grid grid-cols-3 md:grid-cols-4 md:w-4/5">
+      <div className="grid grid-cols-2 md:grid-cols-4 md:w-4/5">
         {filteredProducts.map((product) => {
-          return <ProductCard product={product} />;
+          return <ProductCard product={product} key={product.id} />;
         })}
       </div>
     </div>
