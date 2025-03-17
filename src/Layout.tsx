1e2c8cd0584a6936fa62c87a1952dc98f1f2c973
Young Seo Kim <youngseokim7@gmail.com>
2025-03-17 10:48:25 +0900

    fetch products from api and pass context to children

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,13 +1,33 @@
 import { Outlet } from "react-router-dom";
 import NavbarMobile from "./components/NavbarMobile";
 import NavbarDesktop from "./components/NavbarDesktop";
+import { useEffect, useState } from "react";
+import { Product } from "./types";
 
 export default function Layout() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) {
+          alert(`unable to fetch, error status: ${response.status}`);
+        }
+        const data: Product[] = await response.json();
+        setProducts(data);
+      } catch (error) {
+        alert(`error: ${error}`);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="flex flex-col w-full h-full">
       <NavbarMobile />
       <NavbarDesktop />
-      <Outlet />
+      <Outlet context={products} />
       <div className="mt-auto text-3xl">footer</div>
     </div>
   );
