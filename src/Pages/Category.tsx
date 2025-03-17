10eebf38124f1db002225546ea3d73da8ef41cb5
Young Seo Kim <youngseokim7@gmail.com>
2025-03-17 11:08:47 +0900

    filter by category and initial representation

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,10 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useOutletContext, useParams } from "react-router-dom";
+import { Product } from "../types";
 
 export default function Category() {
   const { category } = useParams();
+  let filterWord = category;
+  if (category === "clothes-m") {
+    filterWord = "men's clothing";
+  } else if (category === "clothes-f") {
+    filterWord = "women's clothing";
+  }
+  const products: Product[] = useOutletContext();
+  const filteredProducts = products.filter((product) => {
+    return product.category === filterWord;
+  });
+  console.log(filterWord);
+
   return (
     <div>
-      <h1>now at {category}</h1>
+      <h1>hello from {category}</h1>
+      {filteredProducts.map((product) => {
+        return (
+          <div key={product.id}>
+            <h1>{product.title}</h1>
+            <h1>{product.price}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
