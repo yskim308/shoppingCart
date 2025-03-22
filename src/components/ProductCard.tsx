c6c884fed755d5a1bc6c7b688389248e31bdafa1
Young Seo Kim <youngseokim7@gmail.com>
2025-03-22 15:46:10 +0900

    link categry cards to item page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Product } from "../types";
 
 interface ProductCardProps {
@@ -6,24 +7,26 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <div className={`p-5 animate-fade`}>
-      <div className="aspect-square w-auto flex flex-col">
-        <img
-          src={product.image}
-          alt="Unable to Load"
-          className={`w-full h-full`}
-        />
+    <Link to={`/${product.category}/${product.id}`}>
+      <div className={`p-5 animate-fade`}>
+        <div className="aspect-square w-auto flex flex-col">
+          <img
+            src={product.image}
+            alt="Unable to Load"
+            className={`w-full h-full`}
+          />
+        </div>
+        <div>
+          <h1>{product.title}</h1>
+        </div>
+        <div>
+          <h1>${product.price}</h1>
+        </div>
+        <div className="flex">
+          <h1>rating: {product.rating.rate}</h1>
+          <h1>reviews: {product.rating.count}</h1>
+        </div>
       </div>
-      <div>
-        <h1>{product.title}</h1>
-      </div>
-      <div>
-        <h1>${product.price}</h1>
-      </div>
-      <div className="flex">
-        <h1>rating: {product.rating.rate}</h1>
-        <h1>reviews: {product.rating.count}</h1>
-      </div>
-    </div>
+    </Link>
   );
 }
