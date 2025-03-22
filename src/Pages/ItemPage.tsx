5cbe4cd2245de5f1b5e49d8afdf254d13332fbb5
Young Seo Kim <youngseokim7@gmail.com>
2025-03-22 16:08:59 +0900

    setup item page to display the correct item

--- src/Pages/ItemPage.tsx
+++ src/Pages/ItemPage.tsx
@@ -1,7 +1,34 @@
+import { useLocation, useOutletContext, useParams } from "react-router-dom";
+import { Product } from "../types";
+import { useEffect, useState } from "react";
+
 export default function ItemPage() {
+  const [itemToDisplay, setItemToDisplay] = useState<Product | null>(null);
+  const products: Product[] = useOutletContext();
+  const { itemId } = useParams();
+
+  // getting in passed state
+  const location = useLocation();
+  const passedItem: Product = location.state;
+
+  useEffect(() => {
+    if (!passedItem && itemId) {
+      // if no item passed, find it in outlet context
+      const foundProduct = products.find(
+        (item: Product) => item.id === parseInt(itemId),
+      );
+      if (foundProduct) setItemToDisplay(foundProduct);
+    } else if (passedItem) {
+      // if item passed, just set it
+      setItemToDisplay(passedItem);
+    } else {
+      alert("item doesn't exist?"); // to be honest, not really sure how this block would ever be triggered
+    }
+  }, [itemId, passedItem, products]);
   return (
-    <div>
-      <h1>this should be the individual page for the items</h1>
+    <div className="flex">
+      <div>{itemToDisplay?.title}</div>
+      <div>{itemToDisplay?.description}</div>
     </div>
   );
 }
