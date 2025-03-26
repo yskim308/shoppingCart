30a3796638ab56a7658414c0fa02644c3ec1ef82
Young Seo Kim <youngseokim7@gmail.com>
2025-03-26 17:56:39 +0900

    finalize alert

--- src/Pages/ItemPage.tsx
+++ src/Pages/ItemPage.tsx
@@ -31,60 +31,62 @@ export default function ItemPage() {
   }, [itemId, passedItem, products]);
 
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
+    <>
       <Alert message="Added to Cart" show={showAlert} />
-      <div className="grid grid-cols-1 md:grid-cols-2 w-4/5 md:w-3/5">
-        <div className="aspect-square">
-          <img
-            src={itemToDisplay?.image}
-            alt="Loading"
-            className="w-full h-full"
-          />
-        </div>
-        <div className="py-10">
-          <h1 className="text-xl font-bold">{itemToDisplay?.title}</h1>
-          <div className="flex justify-between">
-            <div className="flex items-center">
-              <img src={starIcon} alt="star" className="w-5" />
-              <p>{itemToDisplay?.rating.rate}</p>
-              <p className="font-thin mx-1">
-                ({itemToDisplay?.rating.count} reviews)
-              </p>
-            </div>
-            <h1 className="font-bold flex items-center text-green-600">
-              ${itemToDisplay?.price}
-            </h1>
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 w-4/5 md:w-3/5">
+          <div className="aspect-square">
+            <img
+              src={itemToDisplay?.image}
+              alt="Loading"
+              className="w-full h-full"
+            />
           </div>
-          <h1 className="text-xs mt-2">{itemToDisplay?.description}</h1>
-          <form
-            className="flex justify-between mt-5"
-            onSubmit={(e: FormEvent) => {
-              e.preventDefault();
-              if (itemToDisplay) {
-                addCheckoutItem({ product: itemToDisplay, quantity: count });
-              }
-              setShowAlert(true);
-              setTimeout(() => {
-                setShowAlert(false);
-              }, 2000);
-              setCount(0);
-            }}
-          >
-            <div>
-              <label htmlFor="count">Quantity:</label>
-              <input
-                type="number"
-                className="ml-2 w-15 p-2 border border-red-100 "
-                id="count"
-                name="count"
-                value={count}
-                onChange={(e) => setCount(parseInt(e.target.value))}
-              />
+          <div className="py-10">
+            <h1 className="text-xl font-bold">{itemToDisplay?.title}</h1>
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <img src={starIcon} alt="star" className="w-5" />
+                <p>{itemToDisplay?.rating.rate}</p>
+                <p className="font-thin mx-1">
+                  ({itemToDisplay?.rating.count} reviews)
+                </p>
+              </div>
+              <h1 className="font-bold flex items-center text-green-600">
+                ${itemToDisplay?.price}
+              </h1>
             </div>
-            <button type="submit">add to cart</button>
-          </form>
+            <h1 className="text-xs mt-2">{itemToDisplay?.description}</h1>
+            <form
+              className="flex justify-between mt-5"
+              onSubmit={(e: FormEvent) => {
+                e.preventDefault();
+                if (itemToDisplay) {
+                  addCheckoutItem({ product: itemToDisplay, quantity: count });
+                }
+                setShowAlert(true);
+                setTimeout(() => {
+                  setShowAlert(false);
+                }, 2000);
+                setCount(0);
+              }}
+            >
+              <div>
+                <label htmlFor="count">Quantity:</label>
+                <input
+                  type="number"
+                  className="ml-2 w-15 p-2 border border-red-100 "
+                  id="count"
+                  name="count"
+                  value={count}
+                  onChange={(e) => setCount(parseInt(e.target.value))}
+                />
+              </div>
+              <button type="submit">add to cart</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -6,9 +6,11 @@ interface AlertProps {
 export default function Alert({ message, show }: AlertProps) {
   return (
     <div
-      className={`fixed z-40 left-1/2 -translate-x-1/2 p-10 bg-green-300 ease-in-out duration-200 ${show ? "translate-0" : "-translate-y-full"}`}
+      className={`fixed z-40 left-1/2 -translate-x-1/2 ease-in-out duration-200 ${show ? "translate-0" : "-translate-y-full"}`}
     >
-      <p>{message}</p>
+      <div className="p-10 bg-green-50 font-medium rounded-3xl mt-10">
+        {message}
+      </div>
     </div>
   );
 }
