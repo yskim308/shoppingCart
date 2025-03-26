b2c207e2a915b8209afaec9e3581020a5814257c
Young Seo Kim <youngseokim7@gmail.com>
2025-03-26 18:34:40 +0900

    style product cards

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -28,8 +28,8 @@ export default function Category() {
   }, [category, products, filterWord]);
 
   return (
-    <div className="flex justify-center p-10 w-full">
-      <div className="grid grid-cols-2 md:grid-cols-4 md:w-4/5">
+    <div className="flex justify-center lg:p-10 w-full">
+      <div className="grid grid-cols-2 md:grid-cols-3 md:w-4/5 lg:grid-cols-4 lg:w-4/5">
         {filteredProducts.map((product) => {
           return <ProductCard product={product} key={product.id} />;
         })}
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Product } from "../types";
+import starImg from "../assets/star-outline.svg";
 
 interface ProductCardProps {
   product: Product;
@@ -8,7 +9,7 @@ interface ProductCardProps {
 export default function ProductCard({ product }: ProductCardProps) {
   return (
     <Link to={`/${product.category}/${product.id}`} state={product}>
-      <div className={`p-5 animate-fade`}>
+      <div className={`p-2 m-1 lg:m-4 animate-fade shadow-md rounded-lg`}>
         <div className="aspect-square w-auto flex flex-col">
           <img
             src={product.image}
@@ -17,14 +18,19 @@ export default function ProductCard({ product }: ProductCardProps) {
           />
         </div>
         <div>
-          <h1>{product.title}</h1>
+          <h1 className="text-xs lg:text-lg font-bold h-11 lg:h-24 overflow-auto">
+            {product.title}
+          </h1>
         </div>
-        <div>
-          <h1>${product.price}</h1>
-        </div>
-        <div className="flex">
-          <h1>rating: {product.rating.rate}</h1>
-          <h1>reviews: {product.rating.count}</h1>
+        <div className="flex justify-between mt-2">
+          <div className="flex items-center">
+            <img src={starImg} alt="rating" className="w-5 lg:w-7" />
+            <h1 className="text-sm font-medium">{product.rating.rate}</h1>
+            <h1 className="text-sm font-thin"> ({product.rating.count})</h1>
+          </div>
+          <div className="px-2 flex items-center">
+            <h1 className="text-sky-900 font-bold">${product.price}</h1>
+          </div>
         </div>
       </div>
     </Link>
