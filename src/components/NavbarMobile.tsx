9669da5f7813ae0aedcf153fb3627e6a9f8640f8
Young Seo Kim <youngseokim7@gmail.com>
2025-03-26 16:51:02 +0900

    style sidebar

--- src/components/NavbarMobile.tsx
+++ src/components/NavbarMobile.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import CartIcon from "./CartIcon";
 import { NavbarProps } from "../types";
+import menu from "../assets/menu.svg";
 
 interface SidebarProps {
   closeSidebar: () => void;
@@ -24,16 +25,36 @@ function Sidebar({ closeSidebar, isOpen }: SidebarProps) {
         ></div>
       )}
       <div
-        className={`w-3/4 fixed z-30 bg-red-50 h-full ease-out duration-150 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+        className={`w-3/4 fixed z-30  shadow-2xl rounded-tr-xl border-sky-950 bg-sky-50 h-full ease-out duration-150 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
-        <ul className="flex flex-col" onClick={closeSidebar}>
-          <Link className="text-3xl" to="/">
+        <ul className="flex flex-col m-4" onClick={closeSidebar}>
+          <Link className="text-3xl font-medium mb-3" to="/">
             HOME
           </Link>
-          <Link to="electronics">Electronics</Link>
-          <Link to="clothes-f">Women's Clothing</Link>
-          <Link to="clothes-m">Men's Clothing</Link>
-          <Link to="jewelery">jewelery</Link>
+          <Link
+            to="electronics"
+            className="my-2 p-2 shadow text-sky-950 rounded-lg"
+          >
+            Electronics
+          </Link>
+          <Link
+            to="clothes-f"
+            className="my-2 p-2 shadow text-sky-950 rounded-lg"
+          >
+            Women's Clothing
+          </Link>
+          <Link
+            to="clothes-m"
+            className="my-2 p-2 shadow text-sky-950 rounded-lg"
+          >
+            Men's Clothing
+          </Link>
+          <Link
+            to="jeweler"
+            className="my-2 p-2 shadow text-sky-950 rounded-lg"
+          >
+            Jewelery
+          </Link>
         </ul>
       </div>
     </>
@@ -47,14 +68,16 @@ export default function NavbarMobile({ cartSize }: NavbarProps) {
   };
 
   return (
-    <div className="lg:hidden flex justify-between">
-      <div>
-        <button type="button" onClick={() => setShowSideBar(true)}>
-          show sidebar
-        </button>
-      </div>
-      <div>
-        <CartIcon cartSize={cartSize} />
+    <div className="lg:hidden flex bg-sky-50">
+      <div className="flex justify-between p-3 w-full">
+        <div>
+          <button type="button" onClick={() => setShowSideBar(true)}>
+            <img src={menu} alt="menu" className="w-6" />
+          </button>
+        </div>
+        <div>
+          <CartIcon cartSize={cartSize} />
+        </div>
       </div>
       <Sidebar closeSidebar={closeSideBar} isOpen={showSidebar} />
     </div>
