4888dca4370f82385a6a73d35cd675e959a8cb12
Young Seo Kim <youngseokim7@gmail.com>
2025-03-27 11:03:04 +0900

    reduce padding for alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -8,7 +8,7 @@ export default function Alert({ message, show }: AlertProps) {
     <div
       className={`fixed z-40 left-1/2 -translate-x-1/2 ease-in-out duration-200 ${show ? "translate-0" : "-translate-y-full"}`}
     >
-      <div className="p-10 bg-green-50 font-medium rounded-3xl mt-10">
+      <div className="p-5 bg-green-50 font-medium rounded-3xl mt-10">
         {message}
       </div>
     </div>
