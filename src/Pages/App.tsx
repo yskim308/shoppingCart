51ce89ecdea10b2d3847356a9312bbe8788a8f94
Young Seo Kim <youngseokim7@gmail.com>
2025-03-27 11:18:13 +0900

    create basic homepage

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,8 +1,19 @@
 function App() {
   return (
-    <>
-      <h1>this should be the main app</h1>
-    </>
+    <div className="flex justify-center">
+      <div>
+        <div className="p-10">
+          <h1 className="text-xl lg:text-3xl font-bold font-mono text-center">
+            Welcome! click on the navbar to start shoping ;)
+          </h1>
+          <p className="text-sm font-thin font-mono">
+            I didn't design a homepage. I don't know design stuff yet. Maybe
+            someday I will figure out figma and whatnot. Until then, you get
+            plaintext
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
