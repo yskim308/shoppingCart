a19d80bb320350f1b669ea1b778e94c2e6cad7b7
Young Seo Kim <youngseokim7@gmail.com>
2025-03-27 10:52:35 +0900

    add alert when checkout is clicked

--- src/Pages/Checkout.tsx
+++ src/Pages/Checkout.tsx
@@ -1,21 +1,37 @@
 import { useOutletContext } from "react-router-dom";
 import { OutletContextObject } from "../types";
 import ItemCard from "../components/CheckoutItem";
+import { useState } from "react";
+import Alert from "../components/Alert";
 
 export default function Checkout() {
+  const [showAlert, setShowAlert] = useState<boolean>(false);
   const { checkoutItems, clearCheckout }: OutletContextObject =
     useOutletContext();
 
+  const displayAlert = () => {
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+  };
+
+  const checkoutClick = () => {
+    clearCheckout();
+    displayAlert();
+  };
+
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-4/5 m-10">
+    <>
+      <Alert show={showAlert} message="items checked out" />
+      <div className="flex flex-col items-center w-full">
         {checkoutItems.map((item) => {
           return <ItemCard item={item} key={item.product.id} />;
         })}
-        <button type="button" onClick={clearCheckout}>
+        <button type="button" onClick={checkoutClick}>
           CHECKOUT
         </button>
       </div>
-    </div>
+    </>
   );
 }
